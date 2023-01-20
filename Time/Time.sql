f570621a57b0ee6fc9e0699516f1c8cb1029d9a2
Wendel-Fl <wendelfl.dev@gmail.com>
2023-01-19 23:45:01 -0300

    Falta melhorar

--- Time/Time.sql
+++ Time/Time.sql
@@ -1,86 +1,72 @@
-DROP TABLE equipe, uniforme, pais, jogador, partida, campo;
+DROP TABLE pais, equipe, uniforme, jogador, campo, partida;
 
-CREATE TABLE IF NOT EXISTS public.equipe
+CREATE TABLE pais
 (
-	nome character varying (100) COLLATE pg_catalog."default",
-	nome_tecnico character varying (100) COLLATE pg_catalog."default",
-	pais character varying (100) COLLATE pg_catalog."default",
-	CONSTRAINT equipe_pkey PRIMARY KEY (nome),
-	CONSTRAINT equipe_fkey FOREIGN KEY (pais) REFERENCES pais (nome)
-)
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.equipe
-	OWNER to postgres;
-	
-CREATE TABLE IF NOT EXISTS public.uniforme
-(
-	numero integer NOT NULL,
-	cor_camisa character varying (80) COLLATE pg_catalog."default",
-	cor_calcao character varying (10) COLLATE pg_catalog."default",
-	cor_meia character varying (10) COLLATE pg_catalog."default",
-	equipe character varying (10) COLLATE pg_catalog."default",
-	CONSTRAINT uniforme_pkey PRIMARY KEY (numero),
-	CONSTRAINT uniforme_fkey FOREIGN KEY (equipe) REFERENCES equipe (nome)	
-)
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.uniforme
-	OWNER to postgres;
-
-CREATE TABLE IF NOT EXISTS public.pais
-(
-	nome character varying (100) COLLATE pg_catalog."default",
-	continente character varying (100) COLLATE pg_catalog."default",
+	nome character varying (80),
+	continente character varying (80),
 	populacao integer NOT NULL,
-	condicao character varying (80) COLLATE pg_catalog."default",
+	condicao character varying (80),
 	CONSTRAINT pais_pkey PRIMARY KEY (nome)
-)
-TABLESPACE pg_default;
+);
 
-ALTER TABLE IF EXISTS public.pais
-	OWNER to postgres;
-
-CREATE TABLE IF NOT EXISTS public.jogador
+CREATE TABLE equipe
 (
-	nome character varying (100) COLLATE pg_catalog."default",
+	id integer NOT NULL,
+	nome character varying (80),
+	nome_tecnico character varying (80),
+	pais character varying (10),
+	CONSTRAINT equipe_pkey PRIMARY KEY (id),
+	CONSTRAINT equipe_fkey FOREIGN KEY (pais) REFERENCES pais (nome)
+);
+
+CREATE TABLE uniforme
+(
 	numero integer NOT NULL,
-	tipo character varying (10) COLLATE pg_catalog."default",
-	equipe character varying (10) COLLATE pg_catalog."default",
+	cor_camisa character varying (10),
+	cor_calcao character varying (10),
+	cor_meia character varying (10),
+	equipe character varying (10),
+	CONSTRAINT uniforme_pkey PRIMARY KEY (numero),
+	CONSTRAINT uniforme_fkey FOREIGN KEY (equipe) REFERENCES equipe (id)	
+);
+
+CREATE TABLE jogador
+(
+	nome character varying (80),
+	numero integer NOT NULL,
+	tipo character varying (10),
+	equipe integer NOT NULL,
 	CONSTRAINT jogador_pkey PRIMARY KEY (nome),
 	CONSTRAINT jogador_fkey FOREIGN KEY (equipe) REFERENCES equipe (nome)
-)
-TABLESPACE pg_default;
+);
 
-ALTER TABLE IF EXISTS public.jogador
-	OWNER to postgres;
-
-CREATE TABLE IF NOT EXISTS public.partida
+CREATE TABLE campo
 (
-	equipe1 character varying (10) COLLATE pg_catalog."default",
-	equipe2 character varying (10) COLLATE pg_catalog."default",
-	placar character varying (5) COLLATE pg_catalog."default",
-	juiz character varying (100) COLLATE pg_catalog."default",
-	nome_campo character varying (80) COLLATE pg_catalog."default",
-	CONSTRAINT equipe1_pkey PRIMARY KEY (equipe1),
-	CONSTRAINT equipe2_pkey PRIMARY KEY (equipe2),
-	CONSTRAINT nome_campo_pkey PRIMARY KEY (nome_campo),
+	nome_campo character varying (80),
+	cidade_campo character varying (10),
+	CONSTRAINT nome_campo_pkey PRIMARY KEY (nome_campo)
+);
+
+CREATE TABLE partida
+(
+	equipe1 integer NOT NULL,
+	equipe2 integer NOT NULL,
+	placar character varying (5),
+	juiz character varying (10),
+	nome_campo character varying (80),
+	CONSTRAINT equipe1_pkey PRIMARY KEY (equipe1,equipe2, nome_campo),
 	CONSTRAINT partida_equipe1_fkey FOREIGN KEY (equipe1) REFERENCES equipe (nome),
 	CONSTRAINT partida_equipe2_fkey FOREIGN KEY (equipe2) REFERENCES equipe (nome),
 	CONSTRAINT partida_nome_campo_fkey FOREIGN KEY (nome_campo) REFERENCES campo (nome_campo)
-)
-TABLESPACE pg_default;
+);
 
-ALTER TABLE IF EXISTS public.partida
-	OWNER to postgres;
+insert into pais (nome, continente, populacao, condicao) values ('Brasil', 'América do Sul', 214300000, 'Visitante');
+insert into pais (nome, continente, populacao, condicao) values ('Itália', 'Europa', 59000000, 'Anfitrião');
+insert into equipe (id, nome, nome_tecnico, pais) values (1, 'Fluminense', 'Guilherme Schmitz', 'Brasil');
+insert into uniforme (numero, cor_camisa, cor_calcao, cor_meia, equipe) values (1, 'vermelho e verde', 'vermelho', 'branco', 1);
+insert into jogador (nome, numero, tipo, equipe) values ('Lara', '1', 'Central', 1);
+insert into jogador (nome, numero, tipo, equipe) values ('Lara', '1', 'Central', 1);
+insert into jogador (nome, numero, tipo, equipe) values ('Lara', '1', 'Central', 1);
 
-CREATE TABLE IF NOT EXISTS public.campo
-(
-	nome_campo character varying (10) COLLATE pg_catalog."default",
-	cidade_campo character varying (10) COLLATE pg_catalog."default",
-	CONSTRAINT nome_campo_pkey PRIMARY KEY (nome_campo)
-)
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.campo
-	OWNER to postgres;
+insert into campo (nome_campo, cidade_campo) values ('Estádio de Laranjeiras', 'Rio de Janeiro');
+insert into partida (equipe1, equipe2, placar, juiz, nome_campo) values (1, 2, '2x1', 'Anderson Caçador', 'Renato Dall Ara');
